cd53810edabc76dec82a23ce3bc0b65efec2ab1d
Tal Erez <>
2020-05-15 22:13:05 -0700

    Created circuit, added moving camera, fixed camera bug, created horizontal movement for ship, edited terrain resolution to make it higher quality

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x * xOffset;
+        float xPos = Mathf.Clamp(rawXPos, -3f, 3f);
+
+
+        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
