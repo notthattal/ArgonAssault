7d261f6938a0efc1042b4176ba4bf0e7d5712a2a
Tal Erez <>
2020-05-15 22:44:41 -0700

    Added movement to the player's ship and adjusted the camera

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
 {
 
     [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
-    
+    [Tooltip("In m")][SerializeField] float xRange = 5f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +21,15 @@ public class Player : MonoBehaviour
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawXPos = transform.localPosition.x * xOffset;
-        float xPos = Mathf.Clamp(rawXPos, -3f, 3f);
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawYPos, -2.75f, 3.33f);
 
 
-        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
     }
 }
