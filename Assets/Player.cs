01f2e7f730f56e173abbfd59e53adeed31ee753c
Tal Erez <>
2020-05-16 14:27:17 -0700

    Fixed problem that didn't allow ship to pitch properly, added yaw and roll to the ship

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -8,20 +9,51 @@ public class Player : MonoBehaviour
 
     [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
     [Tooltip("In m")][SerializeField] float xRange = 5f;
-    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float ySpeed = 4f;
+
+    [SerializeField] float positionPitchFactor= -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 5f;
+
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
